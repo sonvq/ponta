530c4a0426ee3f82e18461fbeaba38889da8ac22
Baotuyen777 <baotuyen555@gmail.com>
2016-11-26 22:49:01 +0700

    done page3

---
+++ html/js/portfolio.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//let sourceSwap = function () {
+//    let $this = $(this);
+//    let newSource = $this.data('src');
+//    $this.data('src', $this.attr('src'));
+////     $this.fadeOut(0, function () {
+////       
+////        $this.fadeIn(500);
+////    });
+//     $this.attr('src', newSource);
+//};
+//$('.block').find('img').hover(sourceSwap, sourceSwap);
+
+
