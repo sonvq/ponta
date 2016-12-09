38b5e09f88c606ed78e2b800832fb7885857fa32
Baotuyen777 <baotuyen555@gmail.com>
2016-12-10 00:25:17 +0700

    done home page

--- wp-content/themes/ponta.at/home.php
+++ wp-content/themes/ponta.at/home.php
@@ -11,22 +11,7 @@ while (have_posts()): the_post();
             <p class="space-64 visible-md  visible-lg"></p>
             <p class="space-32 visible-xs visible-sm"></p>
             <div class=" collapseText">
-                <p class="textHome">Hi, wir sind OliviaPonta Werbeagentur aus
-                    Kärnten und wir konzipieren, gestalten und entwickeln digitale 
-                    Kommunikation für unsere Kunden.
-                </p>
-                <p class="space-64 visible-md  visible-lg"></p>
-                <p class="space-32 visible-xs visible-sm"></p>
-
-                <p class="textHome">
-                    <a href="#" class="red">Mehr über uns </a>
-                </p>
-                <p class="textHome ">
-                    <a href="#" class="red">Das haben wir bereits gemacht </a>
-                </p>
-                <p class="textHome red">
-                    <a href="#" class="red">Kontakt </a>
-                </p>
+                <?php the_content() ?>
             </div>
         </div>
     </main>
